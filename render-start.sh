ef3b5517a8fcb31a0791a86eb962816fae3bf59e
ai4business-ai <ai4business@mail.ru>
2025-05-17 21:02:44 +0300

    Create render-start.sh

---
+++ render-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is used by Render.com to start the service
+
+# Make sure npm is up to date
+npm --version
+
+# Install dependencies
+npm install
+
+# Start the server
+npm start
